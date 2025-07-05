33eb7892ecb951be34ab305f0d0023edfd052ee9
Sbrentor <githubpersonal914@gmail.com>
2025-07-04 18:51:54 -0600

    redis config

---
+++ src/main/java/saeg/ecommerceback/configuration/Redisconfig.java
@@ -0,0 +1,40 @@
+package saeg.ecommerceback.configuration;
+
+import io.jsonwebtoken.lang.Arrays;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+public class Redisconfig {
+    private final Environment environment;
+
+    public Redisconfig(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(@Value("${REDIS_HOST}") String host,
+                                                         @Value("${REDIS_PORT}") int port,
+                                                         @Value("${REDIS_PASSWORD}") String password) {
+        LettuceConnectionFactory factory = new LettuceConnectionFactory(host, port);
+        if (password != null && !password.isEmpty()) {
+            factory.setPassword(password);
+        }
+
+        if (isProductionProfile()) {
+            factory.setUseSsl(true);
+        }
+
+        return factory;
+    }
+
+    private boolean isProductionProfile() {
+        return Arrays.asList(environment.getActiveProfiles()).contains("prod");
+    }
+}

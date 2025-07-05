d0c9ac9bb71d401cc8be489508501c4468c1f276
Sbrentor <githubpersonal914@gmail.com>
2025-07-04 18:53:19 -0600

    added env vars and commented strings  Api keys

--- src/main/java/saeg/ecommerceback/security/SecurityProperties.java
+++ src/main/java/saeg/ecommerceback/security/SecurityProperties.java
@@ -1,23 +1,32 @@
 package saeg.ecommerceback.security;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
 
 @Data
 @Configuration
-@ConfigurationProperties(prefix = "app.security")
+@ConfigurationProperties(prefix = "ecommerce.security")
 @Validated
 public class SecurityProperties {
     @NotBlank
+    @Value("${JWT_SECRET}")
     private String jwtSecret;
-    @NotBlank
+
+    @NotNull
+    @Positive
+    @Value("${JWT_EXPIRATION}")
     private Long jwtExpiration;
-    @NotBlank
-    private String stipeApiKey;
-    @NotBlank
-    private String sendgripApiKey;
+
+//    @NotBlank
+//    private String stripeApiKey;
+//
+//    @NotBlank
+//    private String sendgridApiKey;
 
 }

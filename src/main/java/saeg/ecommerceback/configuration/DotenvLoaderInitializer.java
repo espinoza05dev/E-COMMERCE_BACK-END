64ecc02419358f03ece211d282cb644e3d1aa09f
Sbrentor <githubpersonal914@gmail.com>
2025-07-09 22:05:03 -0600

    env loader added keys and redis info

--- src/main/java/saeg/ecommerceback/configuration/DotenvLoaderInitializer.java
+++ src/main/java/saeg/ecommerceback/configuration/DotenvLoaderInitializer.java
@@ -22,10 +22,19 @@ public class DotenvLoaderInitializer implements ApplicationContextInitializer<Co
         System.setProperty("REDIS_HOST", dotenv.get("REDIS_HOST"));
         System.setProperty("REDIS_PORT", dotenv.get("REDIS_PORT"));
         System.setProperty("REDIS_PASSWORD", dotenv.get("REDIS_PASSWORD"));
+        System.setProperty("REDIS_USER", dotenv.get("REDIS_USER"));
+        System.setProperty("REDIS_DB", dotenv.get("REDIS_DB"));
+
 
         //jwt
         System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
         System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
         System.setProperty("JWT_DEV_PWD", dotenv.get("JWT_DEV_PWD"));
+
+        //stripe
+        System.setProperty("STRIPE_API_KEY", dotenv.get("STRIPE_API_KEY"));
+
+        //sendgrid
+        System.setProperty("SENDGRID_API_KEY", dotenv.get("SENDGRID_API_KEY"));
     }
 }

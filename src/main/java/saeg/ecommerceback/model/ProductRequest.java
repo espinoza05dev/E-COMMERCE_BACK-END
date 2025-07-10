61fc1339ba06ea57471ac65dba193bebf9dba694
Sbrentor <githubpersonal914@gmail.com>
2025-07-09 22:07:22 -0600

    model modified

--- src/main/java/saeg/ecommerceback/model/ProductRequest.java
+++ src/main/java/saeg/ecommerceback/model/ProductRequest.java
@@ -1,22 +1,40 @@
 package saeg.ecommerceback.model;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Builder
+@Table(name = "products")
 public class ProductRequest {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Integer ProductId;
+    Long ProductId;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Product name is required")
     String ProductName;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Product quantity is required")
+    Long ProductQuantity;
+
     String ProductDescription;
+
     String ProductCurrency;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Product price is required")
     Long ProductPrice;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Product stock is required")
     Long ProductStock;
 }

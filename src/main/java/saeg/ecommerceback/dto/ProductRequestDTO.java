464844807479c489d15a9827a0d59a9e1c388556
Sbrentor <githubpersonal914@gmail.com>
2025-07-09 22:07:28 -0600

    DTO

--- src/main/java/saeg/ecommerceback/dto/ProductRequestDTO.java
+++ src/main/java/saeg/ecommerceback/dto/ProductRequestDTO.java
@@ -1,15 +1,18 @@
 package saeg.ecommerceback.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class ProductRequestDTO {
-    private Integer ProductId;
+    private Long ProductId;
     private String ProductName;
+    private Long ProductQuantity;
     private String ProductDescription;
     private String ProductCurrency;
     private Long ProductPrice;

53e07ff361e28f21063fa119fa0e6baa42545974
Sbrentor <githubpersonal914@gmail.com>
2025-07-12 21:56:48 -0600

    enumes states

---
+++ src/main/java/saeg/ecommerceback/model/StateOrder.java
@@ -0,0 +1,12 @@
+package saeg.ecommerceback.model;
+
+public enum StateOrder {
+    PENDING,
+    CONFIRM,
+    PROCESSING,
+    SENT,
+    IN_TRANSIT,
+    DELIVERED,
+    CANCELED,
+    REFUND,
+}
---
+++ src/main/java/saeg/ecommerceback/model/StatePayment.java
@@ -0,0 +1,9 @@
+package saeg.ecommerceback.model;
+
+public enum StatePayment {
+    PENDING,
+    APPROVED,
+    REJECTED,
+    PARTIAL,
+    REFUND
+}

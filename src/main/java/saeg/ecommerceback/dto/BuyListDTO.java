1ca08019537364d6718d8a8796c893faeb315d8b
Sbrentor <githubpersonal914@gmail.com>
2025-07-15 08:48:17 -0600

    DTO made

--- src/main/java/saeg/ecommerceback/dto/BuyListDTO.java
+++ src/main/java/saeg/ecommerceback/dto/BuyListDTO.java
@@ -1,26 +1,35 @@
 package saeg.ecommerceback.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class BuyListDTO {
-    private String id;
-    private String usuarioId;
-    private List<ItemBuyListDTO> items;
-    private BigDecimal subtotal;
-    private BigDecimal impuestos;
-    private BigDecimal descuentos;
-    private BigDecimal total;
-    private String moneda;
-    private LocalDateTime fechaCreacion;
-    private LocalDateTime fechaModificacion;
+    private Long buylistId;
+    private String buylistusuarioId;
+    private List<ItemBuyListDTO> buylistItems;
+    private BigDecimal buylistSubtotal;
+    private BigDecimal buylistImpuestos;
+    private BigDecimal buylistDescuentos;
+    private BigDecimal buylistTotal;
+    private String buylistMoneda;
+    private LocalDateTime buylistFechaCreacion;
+    private LocalDateTime buylistFechaModificacion;
 //    private EstadoCarrito estado;
-    private String codigoCupon;
+    private String buylistCodigoCupon;
 //    private String direccionEntregaId;
 //    private String metodoEnvioId;
 //    private BigDecimal costoEnvio;
-    private LocalDateTime fechaExpiracion;
-    private String sessionId;
-    private List<String> mensajesValidacion;
+    private LocalDateTime buylistFechaExpiracion;
+    private String buylistSessionId;
+    private List<String> buylistMensajesValidacion;
 }
